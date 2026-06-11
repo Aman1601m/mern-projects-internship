a6c900263cf18e7296f2c18ed13aa3d6247c4652
Santhosh <santhosh0616r@gmail.com>
2026-06-11 20:49:05 +0530

    feat: Create User Authentication Model with Password

--- user.js
+++ user.js
@@ -21,20 +21,27 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Santhosh0616'],
     minlength: 6,
-    select: false 
+    select: false
   }
-}, { timestamps: true });
-
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
+}, 
+{
+  timestamps: true
+});
+UserSchema.pre('save', async function(next){
+  if(!this.isModified('password')){
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(
+    this.password,
+    salt
+  );
+  next();
 });
-
-UserSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+UserSchema.methods.matchPassword = async function(enteredPassword){
+  return await bcrypt.compare(
+    enteredPassword,
+    this.password
+  );
 };
-
 module.exports = mongoose.model('User', UserSchema);
