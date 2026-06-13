541b530438554aec19468231bc1b561ba21493c5
Santhosh <santhosh0616r@gmail.com>
2026-06-13 20:48:10 +0530

    feat: add protected profile route and authentication dependencies

--- routes.js
+++ routes.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {
-    registerUser,
-    loginUser
-}
-=
-require("./controllers");
-router.post(
-    "/register",
-    registerUser
-);
-router.post(
-    "/login",
-    loginUser
-);
-module.exports = router;
 const { protect } = require("./authMiddleware");
 const {
     registerUser,
     loginUser,
     getProfile
 } = require("./controllers");
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.get("/profile", protect, getProfile);
+module.exports = router;
