fdaeada45d8791d8f98d0a46df58ab750d8b9224
Santhosh <santhosh0616r@gmail.com>
2026-06-13 15:54:46 +0530

    feat: add environment variable configuration

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const express = require("express");
-const mongoose = require("mongoose");
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes');
 const app = express();
 app.use(express.json());
-mongoose.connect("mongodb://localhost:27017/hrms")
-.then(()=>{
-    console.log("MongoDB Connected");
-})
-.catch((error)=>{
-    console.log(error);
-});
-app.get("/",(req,res)=>{
-    res.send("HRMS Backend Running");
-});
-const PORT = 5000;
-app.listen(PORT,()=>{
-    console.log(`Server running on ${PORT}`);
-});
+app.use('/api/auth', authRoutes);
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB Connected Successfully!'))
+  .catch(err => console.error('MongoDB Connection Error:', err));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
