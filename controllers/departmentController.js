728332892acf67d364a38027aad4d2a50d4e824b
Aman Shrivastava <shrivastavaaman1601@gmail.com>
2026-06-18 15:10:11 +0530

    test department api endpoints

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -3,6 +3,7 @@ import Department from "../models/Department.js";
 // CREATE
 export const createDepartment = async (req, res, next) => {
   try {
+    console.log("Creating Department:", req.body);
     const dept = await Department.create(req.body);
     res.status(201).json(dept);
   } catch (err) {
@@ -13,6 +14,7 @@ export const createDepartment = async (req, res, next) => {
 // GET ALL
 export const getDepartments = async (req, res, next) => {
   try {
+    console.log("Fetching all departments");
     const depts = await Department.find();
     res.json(depts);
   } catch (err) {
