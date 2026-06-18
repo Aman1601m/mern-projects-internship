be0089ea53da59808c92b57cbce7cc81a3634e5c
Aman Shrivastava <shrivastavaaman1601@gmail.com>
2026-06-18 15:25:34 +0530

    update department routes for update and delete

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -1,9 +1,23 @@
 import express from "express";
-import { createDepartment, getDepartments } from "../controllers/departmentController.js";
+import {
+  createDepartment,
+  getDepartments,
+  updateDepartment,
+  deleteDepartment,
+} from "../controllers/departmentController.js";
 
 const router = express.Router();
 
+// CREATE
 router.post("/", createDepartment);
+
+// GET ALL
 router.get("/", getDepartments);
+
+// UPDATE
+router.put("/:id", updateDepartment);
+
+// DELETE
+router.delete("/:id", deleteDepartment);
 
 export default router;
