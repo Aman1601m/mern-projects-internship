cbd616953fc8671ad34e09c61c590e7876559b4c
Aman Shrivastava <shrivastavaaman1601@gmail.com>
2026-06-22 16:45:10 +0530

    create user model and auth controller

--- models/user.js
+++ models/user.js
@@ -1,14 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: {
-    type: String,
-    enum: ["employee", "hr"],
-    default: "employee",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
